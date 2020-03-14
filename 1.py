695f1e4d1663902d33e978b42db642aa0729e4b0
Tycmoon <42902508+Tycmoon@users.noreply.github.com>
2020-03-14 21:51:58 +0800

    Update 1.py
    统计图

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+
 import csv
 import matplotlib
 import pandas as pd
@@ -5,35 +6,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+df = pd.read_excel('E:\\python\\excel.xlsx')
+total = pd.read_excel('E:\\python\\total.xlsx')
 
+#print(df.fever.value_counts())
 
+#允许显示汉字
+plt.rcParams['font.sans-serif'] = ['SimHei']
 
-def age_Dis():
-    df = pd.read_csv('E:\\python\\excel.csv')
-    print(df)
-    print()
-    age = []
-    for i in df['age']:
-        age1=[]
-        age2=[]
-        age3=[]
-        age4=[]
-        age5=[]
-        for i in age:
-            if 18<=i<25:
-                age1.append(i)
-            elif 25<=i<35:
-                age2.append(i)
-            elif 35<=i<45:
-                age3.append(i)
-            elif 45<=i<55:
-                age4.append(i)
-            else:
-                age5.append(i)
+#窗格大小及标题
+plt.figure(figsize = [10,7])
+plt.suptitle("疫情统计图")
 
-                index=['18~25','25~35','35~45','45~55','others']
-                values=[len(age1),len(age2),len(age3),len(age4),len(age5)]
-                plt.bar(index,values)
-                plt.show()
+#数据
+#num_cols = ['fever','infect','school']
+#print(num_cols)
+#print(df.fever.value_counts())
 
-
+#柱状图
+plt.bar(x = total.index.values,height = total['fever'],color = 'steelblue',
+        label = 'fever',tick_label = ['2020/1/19','2020/1/20','2020/1/21'])
+plt.bar(x = total.index.values,height = total['infect'],color = 'yellow',
+        label = 'infect',bottom = total['fever'],
+        tick_label = ['2020/1/19','2020/1/19','2020/1/21'])
+plt.bar(x = total.index.values,height = total['school'],color = 'red',
+        label = 'school',bottom = total['fever']+total['infect'],
+        tick_label = ['2020/1/19','2020/1/20','2020/1/21'])
+plt.ylabel('人数（人）')
+plt.legend(loc = 'best')
+plt.show()
